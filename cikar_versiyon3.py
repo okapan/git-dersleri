57779e18dd55d09176baed01d46172ee5b0f4284
okapan <oguzkapan@hotmail.com>
2017-07-22 15:05:24 +0300

    versiyon 3 için dosya güncelleme işlemleri yapıldı

--- cikar_versiyon3.py
+++ cikar_versiyon3.py
@@ -1,2 +1,4 @@
 def cikart(x,y):
 	return x-y
+def versiyon3:
+	print("Versiyon 3")
--- main_versiyon3.py
+++ main_versiyon3.py
@@ -2,4 +2,5 @@ def topla(x,y):
 	return x+y
 def hello:
 	print ("Hello")
-	
+def versiyon3:
+	print("Versiyon 3")
